2517ec5ef63583c47845d2c077caa61d4fbeb104
rakvat <rakvat@gmx.net>
2014-05-07 22:38:01 +0200

    getting version from neo4j and test

--- src/neo4j_rest_wrapper.erl
+++ src/neo4j_rest_wrapper.erl
@@ -1,3 +1,24 @@
 -module(neo4j_rest_wrapper).
 -vsn("0.0.1").
 
+-export([start/0, stop/0, dummy/0, get_version/0]).
+
+% are start/stop needed here or is it ok to start inets 
+% in application resource file?
+start() ->
+    inets:start().
+
+stop() ->
+    inets:stop().
+
+dummy() ->
+    "xxx".
+
+get_version() ->
+    {ok, Response} = httpc:request("http://0.0.0.0:7474/db/data/"),
+    {_Status, _Headers, Body} = Response,
+    Parsed = mochijson2:decode(Body),
+    {struct, Data} = Parsed,
+    Version = proplists:get_value(<<"neo4j_version">>, Data),
+    binary_to_list(Version).
+
--- test/neo4j_rest_wrapper_tests.erl
+++ test/neo4j_rest_wrapper_tests.erl
@@ -2,5 +2,18 @@
 -include_lib("eunit/include/eunit.hrl").
 
 basic_logic_test() ->
-    4=2+2,
-    ok.
+    ?assertEqual(4, 2+2).
+
+dummy_test() ->
+    ?assertEqual("xxx", neo4j_rest_wrapper:dummy()).
+
+neo4j_version_test() ->
+    {setup, fun setup/0, fun tear_down/1,
+     ?_test( ?assertEqual("2.0.3", neo4j_rest_wrapper:get_version()))}.
+
+
+setup() ->
+    inets:start().
+tear_down(_) ->
+    inets:stop().
+
