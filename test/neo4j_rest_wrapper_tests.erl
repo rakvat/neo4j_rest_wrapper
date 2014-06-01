9159640156e9d34368ac79bde080e3948d471974
rakvat <rakvat@gmx.net>
2014-06-01 22:52:26 +0200

    started mocking neo4j requests with meck

--- test/neo4j_rest_wrapper_tests.erl
+++ test/neo4j_rest_wrapper_tests.erl
@@ -5,8 +5,9 @@ basic_logic_test() ->
     ?assertEqual(4, 2+2).
 
 with_inets_test_() ->
-    {setup, fun setup/0, fun tear_down/1,
-     [fun neo4j_version/0,
+    {foreach, fun setup/0, fun tear_down/1,
+     [
+      fun neo4j_version/0,
       fun cypher/0,
       fun get_node/0,
       fun create_node/0,
@@ -14,7 +15,12 @@ with_inets_test_() ->
       ]}.
 
 neo4j_version() ->
-    ?assertEqual("2.0.3", neo4j_rest_wrapper:get_version()).
+    FakeJson = "{\"neo4j_version\" : \"2.0.23\"}",
+    FakeResult = {ok, {x,x,FakeJson}},
+    meck:expect(httpc, request, fun(_) -> FakeResult end),
+    ?assertEqual("2.0.23", neo4j_rest_wrapper:get_version()),
+    ?assert(meck:validate(httpc)),
+    ?assertEqual(1, meck:num_calls(httpc, request, '_')).
 
 cypher() ->
     Query="START n=node(*) RETURN n;",
@@ -42,11 +48,17 @@ cypher() ->
     ok.
 
 get_node() ->
-    Result = neo4j_rest_wrapper:get_node(0),
+    FakeJson = "{\"data\" : {\"a\": \"b\"}}",
+    FakeResult = {ok, {x,x,FakeJson}},
+    meck:expect(httpc, request, fun(_) -> FakeResult end),
+    Result = neo4j_rest_wrapper:get_node(1),
     Data = proplists:get_value(<<"data">>, Result),
-    {struct, _Properties} = Data,
-    % [{<<"name">>,<<"rakvat">>}]
-    ok.
+    {struct, Properties} = Data,
+    [{<<"a">>,<<"b">>}] = Properties,
+    ?assert(meck:validate(httpc)),
+    ?assertEqual(1, meck:num_calls(httpc, request, '_')).
+
+% "{\n  \"message\" : \"Cannot find node with id [23] in database.\",\n  \"exception\" : \"NodeNotFoundException\",\n  \"fullname\" : \"org.neo4j.server.rest.web.NodeNotFoundException\",\n  \"stacktrace\" : [ \"org.neo4j.server.rest.web.DatabaseActions.node(DatabaseActions.java:183)\", \"org.neo4j.server.rest.web.DatabaseActions.getNode(DatabaseActions.java:228)\", \"org.neo4j.server.rest.web.RestfulGraphDatabase.getNode(RestfulGraphDatabase.java:265)\", \"java.lang.reflect.Method.invoke(Unknown Source)\", \"org.neo4j.server.rest.transactional.TransactionalRequestDispatcher.dispatch(TransactionalRequestDispatcher.java:139)\", \"org.neo4j.server.rest.security.SecurityFilter.doFilter(SecurityFilter.java:112)\", \"java.lang.Thread.run(Unknown Source)\" ]\n}"
 
 create_node() ->
     Properties = {struct, [{name, <<"rakvat">>}, {age, 77}]},
@@ -56,7 +68,7 @@ create_node() ->
     SortedNodeData = lists:sort(NodeData),
     Expected = [{<<"name">>, <<"rakvat">>}, {<<"age">>, 77}],
     SortedExpected = lists:sort(Expected),
-    SortedExpected = SortedNodeData,
+    ?assertEqual(SortedExpected, SortedNodeData),
     ok.
 
 delete_node() ->
@@ -80,7 +92,9 @@ delete_node() ->
 %   <<"org.neo4j.server.rest.tr"...>>,<<"org.neo4j.server.res"...>>,
 %   <<"java.lang.Thread"...>>]}]
 setup() ->
+    meck:new(httpc, [passthrough]),
     neo4j_rest_wrapper:start("http://0.0.0.0:7474").
 tear_down(_) ->
+    meck:unload(httpc),
     neo4j_rest_wrapper:stop().
 
